42e64b791c6ce1f4083b8ffb72fe41e66c4ac4e4
AMiR Ali VatanParast <aa.vatanparast@gmail.com>
2020-10-26 13:56:09 +0330

    Add static files (css, js, image) and index page

--- arouskhanoum/settings.py
+++ arouskhanoum/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'arouskhanoum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
