95a7601123591af78e952fa2652e9f8d182fcc3b
AMiR Ali VatanParast <aa.vatanparast@gmail.com>
2020-10-26 14:09:07 +0330

    Add missing css map file

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 def index(request):
     return render(request, 'blog/index.html')
+
+
+class IndexPage(TemplateView):
+    template_name = 'blog/index.html'
